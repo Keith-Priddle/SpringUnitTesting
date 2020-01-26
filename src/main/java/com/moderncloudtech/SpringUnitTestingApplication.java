a72b51998e01161c3a6682e7f86d0f94312b8a23
keithpriddle <k.priddle66@gmail.com>
2020-01-26 15:08:13 +0000

    test

--- src/main/java/com/moderncloudtech/SpringUnitTestingApplication.java
+++ src/main/java/com/moderncloudtech/SpringUnitTestingApplication.java
@@ -2,10 +2,18 @@ package com.moderncloudtech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringUnitTestingApplication {
-
+	
+	@RequestMapping("/")
+	public String Home() {
+		
+		return " Hello Docker World!!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringUnitTestingApplication.class, args);
 	}
